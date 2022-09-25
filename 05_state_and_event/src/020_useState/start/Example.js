641c50d01d545a10b7ce23175f78c2bea49b861d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-09-26 08:18:38 +0900

    useState

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,15 @@
-const Example = () => {
+import { useState } from "react"
 
+const Example = () => {
+  let [val, setVal] = useState()
   return (
     <>
-      
+      <input 
+        type="text"
+        onChange={(e)=> {
+          setVal(e.target.value)
+        }}
+      /> = {val}
     </>
   );
 };
