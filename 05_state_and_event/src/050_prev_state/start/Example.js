df273c5dbbf9e82effe4f98a829e967944088ba1
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-09-29 06:35:22 +0900

    Update Example.js

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 const Example = () => {
   const [ count, setCount ] = useState(0)
   const countUp = () => {
-    setCount(count + 1)
+    setCount(count + 1) // 1
+    setCount(count + 1) // 1←2にはならない
     setCount(prevstate => prevstate + 1)
   }
   const countDown = () => {
