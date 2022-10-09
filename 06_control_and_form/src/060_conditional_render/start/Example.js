d2a64c7de5aa16f46005008c663112666485d363
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-10-10 02:03:54 +0900

    条件分岐

--- 06_control_and_form/src/060_conditional_render/start/Example.js
+++ 06_control_and_form/src/060_conditional_render/start/Example.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
@@ -15,13 +15,33 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? "";
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
+
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+            return (
+              <li key={animal}>
+                {
+                  // POINT if文
+                  // if(animal === "Dog") {
+                  //   return <li key={animal}>{animal}★</li>
+                  // } else {
+                  //   return <li key={animal}>{animal}</li>
+                  // }
+                  // POINT 3項演算子
+                  // animal + (animal === "Dog"
+                  //  ? "★"
+                  //  : "")
+                  // POINT null合体演算子
+                  animal ?? "null,undefinedでした"
+                }
+                {/* POINT &&演算子 */}
+                {animal === "Dog" && "★"}
+              </li>
+            );
+          })}
       </ul>
     </>
   );
