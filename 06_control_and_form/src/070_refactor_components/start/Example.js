2897d59f843ba1b402c9695636587e2ac7cd4412
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-10-11 09:57:28 +0900

    コンポーネントの分割

--- 06_control_and_form/src/070_refactor_components/start/Example.js
+++ 06_control_and_form/src/070_refactor_components/start/Example.js
@@ -1,32 +1,21 @@
 import { useState } from "react";
+import AnimalFilter from "./components/animalFilter";
+
+import AnimalList from "./components/animalList";
 
 const Example = () => {
   const animals = ["Dog", "Cat", "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
+  const filteredAnimals = animals.filter((animal) => {
+    const isMatch = animal.indexOf(filterVal) !== -1;
+    return isMatch;
+  })
 
   return (
     <>
-      <input
-        type="text"
-        value={filterVal}
-        onChange={(e) => setFilterVal(e.target.value)}
-      />
-      <ul>
-        {animals
-          .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            return isMatch;
-          })
-          .map((animal) => {
-            return (
-              <li key={animal}>
-                {animal}
-                {animal === "Dog" && "★"}
-              </li>
-            );
-          })}
-      </ul>
+      <AnimalFilter filterState={[filterVal, setFilterVal]} />
+      <AnimalList animals={filteredAnimals}/>
     </>
   );
 };
---
+++ 06_control_and_form/src/070_refactor_components/start/components/animalList.js
@@ -0,0 +1,19 @@
+import AnimalItem from "./animalItem";
+
+const AnimalList = ({ animals }) => {
+  if (animals.length === 0) {
+    return <h3>アニマルが見つかりません。</h3>
+  }
+  return (
+    <ul>
+      {animals
+        .map((animal) => {
+          return (
+            <AnimalItem animal={animal}/>
+          );
+        })}
+    </ul>
+  )
+}
+
+export default AnimalList
