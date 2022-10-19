21ae19eac9fa6154b8ec70fba36c513dad0921e5
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-10-19 09:11:48 +0900

    EnterキーでTodo追加

--- 06_control_and_form/src/130_reminder/start/components/Form.js
+++ 06_control_and_form/src/130_reminder/start/components/Form.js
@@ -3,6 +3,7 @@ import { useState } from "react"
 const Form = ({createTodo}) => {
   const [enteredTodo, setEnteredTodo] = useState("")
   const addTodo = (e) => {
+    e.preventDefault()
     const inputVal = enteredTodo
     const newTodo = {
       id: Math.floor(Math.random() * 1e5),
@@ -12,10 +13,12 @@ const Form = ({createTodo}) => {
     setEnteredTodo("");
   }
   return (
-    <div>
-      <input value={enteredTodo} type="text" onChange={(e) => setEnteredTodo(e.target.value)} />
-      <button onClick={addTodo}>追加</button>
-    </div>
+    <form onSubmit={addTodo}>
+      <div>
+        <input value={enteredTodo} type="text" onChange={(e) => setEnteredTodo(e.target.value)} />
+        <button>追加</button>
+      </div>
+    </form>
   )
 }
 
