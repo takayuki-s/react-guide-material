7a3728630d44ab90a085b93452ea286977a8ed50
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-10-28 09:01:14 +0900

    chakraUI（Listコンポーネント）

--- 07_styling_component/src/050_chakra_ui/start/components/List.js
+++ 07_styling_component/src/050_chakra_ui/start/components/List.js
@@ -1,18 +1,31 @@
+import { StackDivider, VStack, HStack, IconButton, Text } from "@chakra-ui/react"
+import { VscCheck } from "react-icons/vsc"
+
 const List = ({todos, deleteTodo}) => {
     const complete = (id) => {
         deleteTodo(id)
     }
     return (
-        <div>
+        <VStack 
+            divider={<StackDivider />} 
+            width="80%"
+            bgColor="white"
+            // color={{ sm: 'red.600', md: 'blue.600', lg: 'green.500', xl: 'red.600' }}
+            borderColor="blackAlpha.100"
+            borderWidth="1px"
+            borderRadius="3px"
+            p={5}
+            alignItems="start"
+        >
             {todos.map(todo => {
                 return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
+                    <HStack key={todo.id} spacing="5">
+                        <IconButton onClick={() => complete(todo.id)} icon={<VscCheck />} isRound bgColor="cyan.100" opacity="0.5">完了</IconButton>
+                        <Text>{todo.content}</Text>
+                    </HStack>
                 )
             })}
-        </div>
+        </VStack>
     );
 }
 
