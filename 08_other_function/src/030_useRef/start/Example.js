dcb1283e55599c26ab44a014e7799561f2ab0b17
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-10-31 08:52:59 +0900

    refで動画プレイヤーを作成

--- 08_other_function/src/030_useRef/start/Example.js
+++ 08_other_function/src/030_useRef/start/Example.js
@@ -17,10 +17,33 @@ const Case1 = () => {
   );
 };
 
+const Case2 = () => {
+  const [ playing, setPlaying ] = useState(false)
+  const videoRef = useRef()
+  return (
+    <div>
+      <video style={{ maxWidth: "100%"}} ref={videoRef}> 
+        <source src="./sample.mp4"></source>
+      </video>
+      <button onClick={() => {
+        if (playing) {
+          videoRef.current.pause()
+        } else {
+          videoRef.current.play()
+        }
+        setPlaying(prev => !prev)
+      }}>
+        { playing ? 'Stop' : 'Play'}
+      </button>
+    </div>
+  )
+}
+
 const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
     </>
   );
 };
