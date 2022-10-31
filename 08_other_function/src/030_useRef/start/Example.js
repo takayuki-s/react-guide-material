8058691eeb5ee283109f0c54b14b2065231fb3a9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-11-01 06:09:54 +0900

    refとstateの違い
    stateは変更されると再レンダリングされるが、refは変更しても再レンダリングされない

--- 08_other_function/src/030_useRef/start/Example.js
+++ 08_other_function/src/030_useRef/start/Example.js
@@ -22,6 +22,7 @@ const Case2 = () => {
   const videoRef = useRef()
   return (
     <div>
+      <h3>ユースケース2</h3>
       <video style={{ maxWidth: "100%"}} ref={videoRef}> 
         <source src="./sample.mp4"></source>
       </video>
@@ -39,11 +40,41 @@ const Case2 = () => {
   )
 }
 
+const createTimeStamp = () => new Date().getTime();
+const Case3 = () => {
+  const [timeStamp, setValue] = useState(createTimeStamp());
+  const ref = useRef(createTimeStamp());
+
+  const updateState = () => {
+    setValue(createTimeStamp());
+  };
+
+  const updateRef = () => {
+    /* コンソールを見るとブラウザの表示と、ref.currentの中身が異なることを確認できます */
+    ref.current = createTimeStamp();
+    console.log("ref.current -> ", ref.current);
+  };
+  return (
+    <div>
+      <h3>ユースケース3</h3>
+      <p>
+        state: {timeStamp}
+        <button onClick={updateState}>更新</button>
+      </p>
+      <p>
+        ref: {ref.current}
+        <button onClick={updateRef}>更新</button>
+      </p>
+    </div>
+  );
+};
+
 const Example = () => {
   return (
     <>
       <Case1 />
       <Case2 />
+      <Case3 />
     </>
   );
 };
