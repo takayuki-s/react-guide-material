4863e280f304b2adc2b9e24e4e162c8872968c20
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-11-01 19:27:38 +0900

    他のコンポーネントのDOMにアクセス（fowardRef）

--- 08_other_function/src/040_forwardRef/start/Example.js
+++ 08_other_function/src/040_forwardRef/start/Example.js
@@ -1,13 +1,13 @@
 import { useRef, forwardRef } from "react";
 
-const Input = ({ ref }) => {
+const Input = forwardRef((props, ref) => {
   return <input type="text" ref={ref} />
-}
+})
 const Example = () => {
   const ref = useRef();
   return (
     <>
-      <Input customRef={ref}/>
+      <Input ref={ref}/>
       <button onClick={() => ref.current.focus()}>
         インプット要素をフォーカスする
       </button>
