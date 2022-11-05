02b6fb9253b9b08e637a8a3a6b5a48e87bdf6972
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-11-05 09:13:18 +0900

    不変性の保持

--- 10_functional_programming/src/030_immutability/start/Example.js
+++ 10_functional_programming/src/030_immutability/start/Example.js
@@ -1,4 +1,13 @@
 const Example = () => {
+  const num = { val: 2 }
+  const double = (num) => {
+    const newNum = {}
+    newNum.val = num.val * 2
+    return newNum
+  }
+  const newNum = double(num)
+  console.log('newNum', newNum, 'num', num)
+  console.log(newNum === num) // false
   return (
     <>
       <h3>不変性（Immutability）</h3>
