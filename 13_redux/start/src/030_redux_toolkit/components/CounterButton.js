9db42a98c3ef367854a1cf2739cd2006b5be85c0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-11-21 08:45:04 +0900

    Redux ToolkitでReduxを書き換え

--- 13_redux/start/src/030_redux_toolkit/components/CounterButton.js
+++ 13_redux/start/src/030_redux_toolkit/components/CounterButton.js
@@ -4,7 +4,7 @@ import { add, minus } from "../store/modules/counter"
 const CounterButton = ({calcType, step}) => {
     
     const dispatch = useDispatch();
-    
+    console.log(add(step))
     const clickHandler = () => {
         const action = calcType === '+' ? add(step) : minus(step);
         console.log(action)
