926b948ceedc718aabcd7c68c8b34329f7926c03
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-11-26 10:09:52 +0900

    非同期処理のステータスを画面に表示（エラーあり）

--- 13_redux/start/src/060_createAsyncThunk/components/Counter.js
+++ 13_redux/start/src/060_createAsyncThunk/components/Counter.js
@@ -1,15 +1,18 @@
-import { add, minus,addAsync } from "../store/modules/counter"
+import { add, minus,addAsync, addAsyncWithStatus } from "../store/modules/counter"
 
 import CounterResult from "./CounterResult"
 import CounterButton from "./CounterButton"
+import { useSelector } from "react-redux"
 
 const Counter = () => {
+    const status = useSelector(state => state.counter.status)
     return (
         <>
             <CounterResult />
             <CounterButton step={2} calcType="+" actionCreator={add}/>
             <CounterButton step={2} calcType="-" actionCreator={minus}/>
             <CounterButton step={2} calcType="非同期(+)" actionCreator={addAsync}/>
+            <h3>{ addAsyncWithStatus }</h3>
         </>
     )
 }
