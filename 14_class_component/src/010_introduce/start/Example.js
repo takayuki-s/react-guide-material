622d749d3a1d80606166398501eb7803031db189
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-11-30 08:50:02 +0900

    クラスコンポーネントの定義

--- 14_class_component/src/010_introduce/start/Example.js
+++ 14_class_component/src/010_introduce/start/Example.js
@@ -1,7 +1,17 @@
-const Example = () => {
-  return (
-    <h3>I'm Function Component</h3>
-  );
-};
+import { Component } from 'react'
+
+class Example extends Component {
+  render() {
+    return (
+      <h3>I'm Class Component</h3>
+    )
+  }
+}
+
+// const Example = () => {
+//   return (
+//     <h3>I'm Function Component</h3>
+//   );
+// };
 
 export default Example;
