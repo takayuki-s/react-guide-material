a5a84fbbec36a1e17ef374fc0cddf31a96702651
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-12-01 07:04:06 +0900

    クラスコンポーネントでの状態管理

--- 14_class_component/src/030_state/start/Example.js
+++ 14_class_component/src/030_state/start/Example.js
@@ -1,6 +1,37 @@
+import { Component } from "react";
+
+class SwitchButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchOn: true,
+      label: 'On'
+    };
+    this.changeSwitch = this.changeSwitch.bind(this)
+  }
+
+  changeSwitch() {
+    console.log(this)
+    this.setState(prevState => {
+      return {
+        switchOn: !prevState.switchOn,
+        label: !prevState.switchOn ? 'On' : 'Off'
+      }
+    })
+  }
+
+  render() {
+    const { label } = this.state
+    return (
+      <button onClick={this.changeSwitch}>{label}</button>
+    )
+  }
+}
+
 const Example = () => {
   return (
     <>
+      <SwitchButton />
     </>
   );
 };
