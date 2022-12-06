0366349e46fb36ffc24e95598dacfbfbae5da465
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-12-06 09:58:21 +0900

    Axiosを使ってサーバーからデータを取得
    useEffect内で直接asyncを記述してはいけない

--- 16_rest_api/src/040_axios_get_request/start/Example.js
+++ 16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const Example = () => {
-  return <></>;
+  useEffect(() => {
+    // .thenを使う場合
+    // axios.get('http://localhost:3003/user').then((res) => {
+      //   console.log(res.data)
+      // })
+
+    // async awaitを使う場合
+    const getUser = async () => {
+      const res = await axios.get('http://localhost:3003/user')
+      console.log(res.data)
+    }
+    getUser()
+  })
 };
 
 export default Example;
