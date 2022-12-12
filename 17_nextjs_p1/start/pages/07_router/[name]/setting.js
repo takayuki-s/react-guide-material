4da5ce2e498ffa68089d0db7ecc638e79f353baf
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-12-12 09:06:20 +0900

    指定されたパスの値をJSで取得

--- 17_nextjs_p1/start/pages/07_router/[name]/setting.js
+++ 17_nextjs_p1/start/pages/07_router/[name]/setting.js
@@ -1,8 +1,18 @@
 
 import { useRouter } from "next/router";
 
-export default function Setting() {
+export default function Setting({ query }) {
+  const router = useRouter();
   return (
-    <h1>[name]/setting.js</h1>
+    <>
+        <h1>routerから取得: {router.query.name}</h1>
+        <h1>getServerSidePropsから取得: {query.name}</h1>
+    </>
   )
+}
+
+export async function getServerSideProps({ query }) {
+    return {
+        props: { query }
+    }
 }
