b557a33f7de21f2bb9e84dbef05e4fec6a735aff
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-12-13 07:58:24 +0900

    useRouterを使って画面遷移

--- 17_nextjs_p1/start/pages/07_router/[name]/setting.js
+++ 17_nextjs_p1/start/pages/07_router/[name]/setting.js
@@ -3,10 +3,17 @@ import { useRouter } from "next/router";
 
 export default function Setting({ query }) {
   const router = useRouter();
+  console.log(router.query)
+  const clickHandler = () => {
+    // router.push('/', '/dummy-url')
+    // router.replace('/', '/dummy-url')
+    router.reload()
+  }
   return (
     <>
         <h1>routerから取得: {router.query.name}</h1>
         <h1>getServerSidePropsから取得: {query.name}</h1>
+        <button onClick={clickHandler}>アクションによる画面遷移</button>
     </>
   )
 }
