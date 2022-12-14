c82e9e5bfa9b071a68b362d607e46f9aaae259a2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-12-15 08:34:15 +0900

    Linkを使った画面遷移
    Linkコンポーネントを使うとブラウザの更新が行われない

--- 17_nextjs_p1/start/pages/index.js
+++ 17_nextjs_p1/start/pages/index.js
@@ -4,6 +4,15 @@ export default function Home() {
   return (
     <>
       <h1>Home</h1>
+      <Link href="/07_router" as="dummy-url">
+        <a>/07_router</a>
+      </Link>
+      <Link href={{ pathname: "/07_router", query: { key: "value" }}}>
+        <a>/07_router</a>
+      </Link>
+      <Link href="/07_router/hello/setting">
+        <a>/07_router</a>
+      </Link>
     </>
   )
 }
