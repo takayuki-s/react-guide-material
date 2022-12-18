7d99076b28377e0979349b0a82ac0b1d5a0368a6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-12-18 11:07:43 +0900

    サイト全体にレイアウトを適用

--- 17_nextjs_p1/start/pages/_app.js
+++ 17_nextjs_p1/start/pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import { AppProvider } from "../context/AppContext";
+import Layout1 from "../components/layout/layout1"
 
 function MyApp({ Component, pageProps }) {
   return (
     <AppProvider>
-      {<Component {...pageProps} />}
+      <Layout1>
+        {<Component {...pageProps} />}
+      </Layout1>
     </AppProvider>
   )
 }
