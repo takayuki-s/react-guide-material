2890bbfbd17a55ab58a6aab87ae6cee6db917181
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-12-19 06:59:21 +0900

    サイト全体レイアウトの出し分け

--- 17_nextjs_p1/start/pages/_app.js
+++ 17_nextjs_p1/start/pages/_app.js
@@ -3,11 +3,11 @@ import { AppProvider } from "../context/AppContext";
 import Layout1 from "../components/layout/layout1"
 
 function MyApp({ Component, pageProps }) {
+  const getLayout = Component.getLayout ?? ((page) => 
+  <Layout1>{page}</Layout1>)
   return (
     <AppProvider>
-      <Layout1>
-        {<Component {...pageProps} />}
-      </Layout1>
+      {getLayout(<Component {...pageProps} />)}
     </AppProvider>
   )
 }
