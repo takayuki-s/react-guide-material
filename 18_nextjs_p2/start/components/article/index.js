c05a8608da7dbf852b07a7d5dcee0bf85ee837c2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-12-26 09:36:19 +0900

    フォルダ配置再々修正

--- 18_nextjs_p2/start/components/article/index.js
+++
@@ -1,9 +0,0 @@
-export default function Article({ data }) {
-  return (
-    <div className="container">
-      <h1 className="title">{data.title}</h1>
-      <p className="text">{data.text}</p>
-      <div className="date">投稿日: {data.date}</div>
-    </div>
-  );
-}
--- 18_nextjs_p2/start/data/nav.js
+++
@@ -1,6 +0,0 @@
-export const navList = [
-  "010_SSR",
-  "020_SG",
-  "030_SG_fetch",
-  "040_api_routes",
-];
