9fe3ac8f550eaefec1a2dc2f2db8c2481437456e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-12-27 19:07:17 +0900

    SSR挙動

--- 18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ 18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react"
+
 export default function SSR() {
     console.log('hello')
+    useEffect(() => {
+        console.log('useEffect');
+        window.localStorage.setItem('key', 'value');
+        document.cookie = 'val=0; path=/;'
+    }, [])
+
+    const [state, setState] = useState('bye');
+    const val = 0
+    return (
+        <>
+            <h3>SSR</h3>
+            <h3>{state}</h3>
+        </>
+    )
 }
