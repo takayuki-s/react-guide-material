dd92596aa5cf4b98af92d96c1c0dd7bc1a3df467
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-01-09 09:38:31 +0900

    テストコードの書き方の基礎を学ぼう

--- 19_test/src/030_how_to_test/start/components/Counter.test.js
+++ 19_test/src/030_how_to_test/start/components/Counter.test.js
@@ -3,4 +3,22 @@ Arrage:(テストデータ、条件、環境の設定).
 Act:(ロジックの実行、関数の実行).
 Assertion:(実行結果と期待する結果の比較).
 */
-test('ダミーテスト:削除して記載してください。', () => {});
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "./Counter";
+
+test('「カウントアップ」ボタン押下で「現在のカウント」が+1されるか', () => {
+  // Arrange
+  render(<Counter originCount={0} />)
+
+  // 更新前
+  const spanElBeforeUpdate = screen.getByText('現在のカウント:0')
+  expect(spanElBeforeUpdate).toBeInTheDocument()
+
+  // Act
+  const btn = screen.getByRole('button', { name: 'カウントアップ' })
+  fireEvent.click(btn)
+
+  // Assertion
+  const spanEl = screen.getByText('現在のカウント:1')
+  expect(spanEl).toBeInTheDocument()
+});
