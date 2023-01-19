482f9ab5d926ffbe31956f316a3ea74f12d6e425
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-01-19 09:09:58 +0900

    プリミティブな値の型定義

--- 20_typescript/src/030_primitives/start/Example.tsx
+++ 20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,30 @@
 const Example = () => {
-  // 練習用
+  let str: string = 'Hello';
+  str = 'Bye';
+  console.log(str);
+
+  let num: number = 102
+  console.log(num)
+
+  let bignum: bigint = 102n
+  console.log(bignum)
+
+  let bool: boolean = true
+  console.log(bool)
+
+  let nullish: null = null
+  console.log(nullish)
+  
+  let undefinedValue: undefined = undefined;
+  console.log(undefinedValue)
+
+  // リテラル型
+  let trueVal: true = true;
+  let num123: 123 = 123;
+  let strHello: 'Hello' = 'Hello';
+
+  // any型(何でも格納可能)
+  let anyValue: any = [];
 };
 
 export default Example;
