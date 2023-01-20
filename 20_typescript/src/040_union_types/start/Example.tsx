093e8be81c12bdc8b232e440084f23860fe288c5
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-01-20 09:42:27 +0900

    ユニオン型の定義

--- 20_typescript/src/040_union_types/start/Example.tsx
+++ 20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,22 @@
 const Example = () => {
-  // 練習用
+  let strOrNum: string | number = 'Hello';
+  strOrNum = 123;
+  console.log(strOrNum);
+  
+  let strOrNumOrBool: string | number | boolean = 'Hello';
+  strOrNumOrBool = true;
+  console.log(strOrNumOrBool);
+
+  let helloOrNumOrBool: 'Hello' | number | boolean = false;
+
+  type HelloOrNum = 'Hello' | number;
+  const hello: HelloOrNum = 'Hello';
+
+  type DayOfWeek =
+    | 'Monday' // 最初の要素に"/"をつけても省略されるが、formatなどで見やすいためにつけている
+    | 'Tuesday'
+
+  const day: DayOfWeek = 'Monday';
 };
 
 export default Example;
