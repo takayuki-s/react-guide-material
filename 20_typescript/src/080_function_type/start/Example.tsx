683a8fde49264044782a505178be0d67a6acec39
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-01-22 09:41:24 +0900

    関数の型定義

--- 20_typescript/src/080_function_type/start/Example.tsx
+++ 20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,17 @@
 const Example = () => {
-  // 練習用
+  function sum1(x: number, y:number) {
+    return x + y
+  }
+  const result1 = sum1(1,2)
+  console.log(result1)
+
+  const sum2 = (x: number, y:number): number => x + y
+  const result2 = sum2(10,20)
+
+  type Sum = (x: number, y:number) => number
+  const sum3: Sum = (x, y) => x + y
+  const result3 = sum3(1,2)
+  console.log(result3)
 };
 
 export default Example;
