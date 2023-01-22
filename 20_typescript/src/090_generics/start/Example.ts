b964a5544b7586e48274b7d3f4b0752c30ad9fe9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-01-23 06:27:09 +0900

    ジェネリクス
    ジェネリクス…型引数(type parameters)を受け取る関数を作る機能

--- 20_typescript/src/090_generics/start/Example.ts
+++ 20_typescript/src/090_generics/start/Example.ts
@@ -1,5 +1,28 @@
 const Example = () => {
-  // 練習用
+  const repeatStr = (value: string, times: number): string[] => {
+    return new Array(times).fill(value)
+  }
+  const repeatNum = (value: number, times: number): number[] => {
+    return new Array(times).fill(value)
+  }
+  const repeat = <T>(value: T, times: number): T[] => {
+    return new Array(times).fill(value)
+  }
+
+  const strArry = repeatStr("hello", 3)
+  console.log(strArry)
+
+  const numArry = repeatNum(10, 3)
+  console.log(numArry)
+
+  const strArry2 = repeat<String>('hello', 3)
+  console.log(strArry2)
+
+  const numArry2 = repeat<Number>(10, 3)
+  console.log(numArry2)
+
+  const boolArry = repeat<Boolean>(true, 3)
+  console.log(boolArry)
 };
 
 export default Example;
