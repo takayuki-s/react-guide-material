58d5e19a14df59f7109d057f52b3390c10905609
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-01-26 08:23:10 +0900

    classの型定義

--- 20_typescript/src/130_class/start/Example.tsx
+++ 20_typescript/src/130_class/start/Example.tsx
@@ -1,5 +1,24 @@
+class User {
+  public name: string;
+  private age: number;
+  // protected age: number // protected←クラス内や継承したクラス内でしか使えない
+
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+}
+
 const Example = () => {
-  // 練習用
+  const user = { name: "Hanako", age: 21 };
+  const user1 = new User("Hanako", 22);
+  console.log(user1)
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+    </div>
+  );
 };
 
 export default Example;
