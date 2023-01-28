989b5463c032fe8f4ff3e182e0987c78b5101bbd
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-01-28 11:51:37 +0900

    useStateの型定義

--- 20_typescript/src/150_usestate/start/Example.tsx
+++ 20_typescript/src/150_usestate/start/Example.tsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
 
+type Todo = {
+  id: number;
+  text: string;
+};
+
+type User = {
+  name: string;
+  age: number;
+};
+
 const Example = () => {
-  
+  const [text, setText] = useState("hello");
+  useState(0);
+
+  const [animals, setAnimals] = useState<string[]>(["dog", "cat"]);
+  const [users, setUsers] = useState<User[]>([{ name: "Tom", age: 22 }]);
+  const [todos, setTodos] = useState<Todo[]>([{ id: 0, text: "Homework" }]);
 };
 
 export default Example;
