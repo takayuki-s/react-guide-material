f4122b3b77b8aa6309f405d948cafb2b9247750f
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-01-30 09:02:47 +0900

    Todoリスト

--- 20_typescript/src/160_add_list_item/start/Example.tsx
+++ 20_typescript/src/160_add_list_item/start/Example.tsx
@@ -1,7 +1,36 @@
 import { useState } from "react";
+import { TaskList } from "./TaskList";
 
-const Example = () => {
-  
+export type Todo = {
+  id: number;
+  text: string;
+};
+
+const Example: React.FC = () => {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, text: "Todo1" },
+    { id: 2, text: "Todo2" },
+  ]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleClick = () => {
+    setTodos([...todos, { id: todos.length + 1, text: inputText }]);
+    setInputText("");
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputText} onChange={handleChange} />
+      <button type="button" onClick={handleClick}>
+        追加
+      </button>
+      <TaskList todos={todos} />
+    </div>
+  );
 };
 
 export default Example;
